06152d65e4b9e02656aebfd9f7b13215e7520cc4
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-08-06 14:24:53 -0600

    [RFR] WsRequest, se modificaron algunas funciones

--- core/wsrequest.py
+++ core/wsrequest.py
@@ -1,23 +1,40 @@
-from config.py import ATTR_SEPARTOR
-
-class WsRequest:
-
-    def __init__():
-
-    def parse_headers(self, str_headers):
-        headers = str_headers.split("ATTR_SEPARATOR") 
-        dict_header = dict()
-
-        for header in headers:
-            index, value = header.split(":")
-            dict_header[index] = value
-
-        dict_headers = #TODO implementación que dará valor
-        clean_headers(dict_headers);
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 
-    def get_event_data(self, event)
-        return args['clazz'], args['method'], args['args']
+class WsRequest():
 
 
-    def clean_headers(arr_headers):
+    def split_sections(string):
+        headers, body = string.split("\n\n", 1) 
+        return headers, body
+
+
+    def parse_request(request):
+        str_header, body = WsRequest.split_sections(request)
+        dict_headers = WsRequest.parse_headers(str_header)
+        return dict_headers, body
+
+    def parse_headers(str_headers):
+        arr_headers = WsRequest.__split_headers(str_headers)
+        dict = WsRequest.__arrheaders_to_dict(arr_headers)
+        return dict
+
+
+    def parse_header(header):
+        index, value = header.split(":")
+        return  index, value
+
+
+    def __split_headers(str_headers):
+        headers = str_headers.split("\n")
+        return headers
+
+
+    def __arrheaders_to_dict(arr_headers):
+        dict_headers = dict()
+        for header in arr_headers:
+            index, value = WsRequest.parse_header(header)
+            dict_headers[index] = value
+        return dict_headers
