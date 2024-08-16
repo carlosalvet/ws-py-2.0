c7724f88f8d398e3e57669da0cea91bc921ca445
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-08-16 11:41:16 -0600

    [FIX]MOdificando helper/user, para user_new y generar el usuario usando el helper

--- helpers/user.py
+++ helpers/user.py
@@ -3,13 +3,14 @@ from models.user.citizen import WS_Citizen
 from models.user.expert import WS_Expert
 from models.user.visual import WS_Visual 
 
-def user_instance_of(_type='', username='', password=''):
+def user_new_instance(_type='', username='', password=''):
     user = None
     if(_type == 'expert'):
         user = WS_Expert(username, password)
     elif(_type == 'admin'):
         user = WS_Admin()
     elif(_type == 'citizen'):
+        print('[DEBUG]', 'Nuevo usuario (CITIZEN)', end=" ")
         user = WS_Citizen()
     else:
         user = WS_Visual()
@@ -18,7 +19,8 @@ def user_instance_of(_type='', username='', password=''):
 
 # para user_id se usa el fd (file descriptor)
 def user_new(user_id, chat_id, type=''):
-    user = user_instance_of(type)
+    user = user_new_instance(type)
     user.id = user_id 
     user.chat_id = chat_id
+    print('user: ', user) 
     return user
