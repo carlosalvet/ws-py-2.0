6f38a5618c55765501cfef52f9fbeda6587e758b
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-09-18 12:14:52 -0600

    [RFR] Refactorizando nombres de variables en wsrequest, cambió ATTR_SEPARATOR Por HEADER_NAME_SEPARATOR

--- config/app.py
+++ config/app.py
@@ -1,13 +1,21 @@
+import os
+
+
 __events__ = ["events.session", "events.htmlrequest", "events.chat", "events.sessions", "events.chat", "events.message"]
 
 CHAT_ID_MAX_SIZE = 320000
 
-SECTION_SEPARATOR = "\n\n"
-HEADER_SEPARTOR = "\n"
-ATTR_SEPARTOR = ":"
 
 BASE_URL = "/home/carlos/proyectos-py/ws-chat2.0/app"
-
 CHAT_DIR = '../private/chat-edomex/chat'
-CHAT_BASENAME = 'conversation.log'
 CACHE_DIR = '../private/var'
+
+
+# Constantes en Petición del cliente
+SECTION_SEPARATOR = "\n\n"
+HEADER_SEPARTOR = "\n"
+HEADER_NAME_SEPARATOR = ":"
+    
+CHAT_BASENAME = 'conversation.log'
+CHAT_DATA_FILENAME = 'metadata.ini'
+print("CHAT_DIR: ", CHAT_DIR)
