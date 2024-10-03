45fd431efd6a24dd349e51938bfceb62fab21f1c
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-10-03 09:56:14 -0600

    [CHG]arr_ini_contents(filename) ahora usa la libreria ConfigParser para leer el archivo INI

--- core/arrays.py
+++ core/arrays.py
@@ -3,11 +3,23 @@ from core.console import console_log
 
 
 def arr_ini_contents(filename):
+    console_log(f'core.console.arr_ini_contents filename: {filename}', 1)
+    contents = []
     config = configparser.ConfigParser()
-    config.read(filename)
+
+    try:
+        config.read(filename)
+        config.sections()
+        contents = config
+    except configparser.MissingSectionHeaderError as e:
+        console_log(f"Error: El archivo INI no contiene secciones válidas. Detalles: {e}", 4)
+    except FileNotFoundError:
+        console_log(f"Error: El archivo INI no fue encontrado: {filename}.", 4)
+    except Exception as e:
+        console_log(f"Ocurrió un error inesperado: {e}", 4)
 
     console_log(f'obteniendo chat desde filename: {filename}', 2)
-    return config
+    return contents
 
 
 def str_to_array(string):
