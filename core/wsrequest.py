cd8fa0549e58e4524c9fe10fd906779aa97c932b
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-10-10 16:59:56 -0600

    [FIX]Test modificaciones y eliminación de bugs

--- core/wsrequest.py
+++ core/wsrequest.py
@@ -21,28 +21,30 @@ class WsRequest():
     """
     @staticmethod
     def split(request):
-        if not request: return ('', '')
         str_header, body = WsRequest.split_sections(request)
-        dict_headers = WsRequest.format_headers(str_header)
+        dict_headers = WsRequest.dict_headers(str_header)
         return dict_headers, body
 
     @staticmethod
-    def format_headers(str_headers):
-        arr_headers = WsRequest.__split_headers(str_headers)
-        dict = WsRequest.__arrheaders_to_dict(arr_headers)
-        return dict
-
-
-    def __split_headers(str_headers):
+    def dict_headers(str_headers):
         arr_headers = str_headers.split(HEADER_SEPARTOR)
-        return arr_headers
+        dict_headers = WsRequest.__arrheaders_to_dict(arr_headers)
+        return dict_headers
 
 
+    """
+        Se ingresa la cadena de texto del request que corresponde a las 
+        cabeceras, y esta la guarda en un diccionario donde cada registro
+        es el elemento separado por \n y la clave y valor son los que estón
+        separados por ':'
+        @str_headers 'Header1:value1\nHeader2:value2'
+        @return arr {'Header1':'value1',·'Header2':'value2'}
+    """
     def __arrheaders_to_dict(arr_headers):
         dict_headers = dict()
         for header in arr_headers:
             index, value = WsRequest.parse_header(header)
-            dict_headers[index] = value
+            if index and value: dict_headers[index] = value
         return dict_headers
 
 
