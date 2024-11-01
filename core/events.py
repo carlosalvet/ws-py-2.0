4dfae368ed60d8e981f95754b1f3eaf2f1877f5d
Carlos Sánchez <carlos.sanchez@edomex.gob.mx>
2024-10-31 18:10:17 -0600

    [FIX]Agregando saneamiento de dato event_data

--- core/events.py
+++ core/events.py
@@ -3,26 +3,27 @@ from core.router import Router
 from core.wsrequest import WsRequest
 from core.eventdispatcher import EventDispatcher 
 from events.base import pre_event_data
-from security.funcs import filter_event_code
+from security.funcs import filter_event_code, clean_headers
 from core.console import console_log
 
 
-async def call_event(websocket, request, extra_data=None):
+async def call_event(websocket, request, open_data=None):
     console_log('-----------------------------------------', 0)
     console_log(f'core.events.call_event request: {request} ', 3)
-    console_log(f'core.events.call_event extra_data: {extra_data}, ', 3)
+    console_log(f'core.events.call_event open_data: {open_data}, ', 3)
     response = {'event':'', 'status': 500}
     websocket_id = id(websocket)
-    dict_headers, body = WsRequest.split(request)
-    event_code = dict_headers["event"] if 'event' in dict_headers else ''
+    headers, body = WsRequest.split(request)
+    event_code = headers.pop("event") if 'event' in headers else ''
 
-    clean_code = filter_event_code(event_code)
-    if event_code:
-        event_data = pre_event_data(dict_headers, extra_data) 
-        response = EventDispatcher.call(event_code, body, event_data)
-    else: response['message'] = f'Petición incorrecta, código de evento no válido: "{event_code}"'
+    cls_event_code = filter_event_code(event_code)
+    cls_headers = clean_headers(headers)
+    console_log(f'event_code: {event_code}, clean code: {cls_event_code}')
+    if cls_event_code:
+        event_data = pre_event_data(cls_event_code, headers, open_data) 
+        response = EventDispatcher.call(cls_event_code, body, event_data)
+    else: 
+        response['message'] = f'Petición incorrecta, código de evento no válido: "{cls_event_code}"'
 
     str_response = Router.stringify(response)
     await websocket.send(str_response)
-
-
